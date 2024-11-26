9b68df2a32135c0a964bd440339f6c9c3966e107
Yuji Hanamura <yuji.hanamura@readyfor.jp>
2024-11-26 21:06:56 +0900

    Use SolidQueue, SolidCache and SolidCable on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,8 @@ Rails.application.configure do
   end
 
   # Change to :null_store to avoid any caching.
-  config.cache_store = :memory_store
+  # config.cache_store = :memory_store
+  config.cache_store = :solid_cache_store
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -42,6 +43,9 @@ Rails.application.configure do
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
+
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
