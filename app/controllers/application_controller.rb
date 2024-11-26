16766af5c4c9e613d97173240eef222396114842
Yuji Hanamura <yuji.hanamura@readyfor.jp>
2024-11-26 21:06:56 +0900

    Add dashboard view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def current_account
+    rodauth.rails_account
+  end
+  helper_method :current_account
 end
